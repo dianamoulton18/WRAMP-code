05c9c467151100500dc2065875d61d623f6bb310
dianamoulton18 <78879769+dianamoulton18@users.noreply.github.com>
2021-03-15 22:05:26 -0400

    image cropping + insertion

--- WRAMP Project/button_imp/button_script.js
+++ WRAMP Project/button_imp/button_script.js
@@ -9,102 +9,103 @@ function pageChanger(){
     switch(buttonNum){
         case 2:
             document.getElementById("questionHeader").innerHTML = "Question 2/21";
-            document.getElementById("imgClickAndChange").src = "C:\Users\marit\OneDrive\Documents\GitHub\WRAMP-code\images\pie\pie-10.png";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-05.png?raw=true";
             sessionStorage.setItem("buttonClicks", "3")
             break;
         case 3:
             document.getElementById("questionHeader").innerHTML = "Question 3/21";
-            document.getElementById("imgClickAndChange").src = "C:\Users\marit\OneDrive\Documents\GitHub\WRAMP-code\images\pie\pie-15.png";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-10.png?raw=true";
             sessionStorage.setItem("buttonClicks", "4")
             break;
         case 4:
             document.getElementById("questionHeader").innerHTML = "Question 4/21";
-            document.getElementById("imgClickAndChange").src = "C:\Users\marit\OneDrive\Documents\GitHub\WRAMP-code\images\pie\pie-20.png";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-15.png?raw=true";
             sessionStorage.setItem("buttonClicks", "5")
             break;
         case 5:
             document.getElementById("questionHeader").innerHTML = "Question 5/21";
-            document.getElementById("imgClickAndChange").src = "data5";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-20.png?raw=true";
             sessionStorage.setItem("buttonClicks", "6")
             break;
         case 6:
             document.getElementById("questionHeader").innerHTML = "Question 6/21";
-            //document.getElementById("imgClickAndChange").src = "data6";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-25.png?raw=true";
             sessionStorage.setItem("buttonClicks", "7")
             break;
         case 7:
             document.getElementById("questionHeader").innerHTML = "Question 7/21";
-            //document.getElementById("imgClickAndChange").src = "data7";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-30.png?raw=true";
             sessionStorage.setItem("buttonClicks", "8")
             break;
         case 8:
             document.getElementById("questionHeader").innerHTML = "Question 8/21";
-            //document.getElementById("imgClickAndChange").src = "data8";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-35.png?raw=true";
             sessionStorage.setItem("buttonClicks", "9")
             break;
         case 9:
-            //document.getElementById("imgClickAndChange").src = "data9";
+            document.getElementById("questionHeader").innerHTML = "Question 9/21";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-40.png?raw=true";
             sessionStorage.setItem("buttonClicks", "10")
             break;
         case 10:
             document.getElementById("questionHeader").innerHTML = "Question 10/21";
-            //document.getElementById("imgClickAndChange").src = "data10";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-45.png?raw=true";
             sessionStorage.setItem("buttonClicks", "11")
             break;
         case 11:
             document.getElementById("questionHeader").innerHTML = "Question 11/21";
-            //document.getElementById("imgClickAndChange").src = "data11";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-50.png?raw=true";
             sessionStorage.setItem("buttonClicks", "12")
             break;
         case 12:
             document.getElementById("questionHeader").innerHTML = "Question 12/21";
-            //document.getElementById("imgClickAndChange").src = "data12";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-55.png?raw=true";
             sessionStorage.setItem("buttonClicks", "13")
-            break;            document.getElementById("questionHeader").innerHTML = "Question 9/21";
+            break;            
 
         case 13:
             document.getElementById("questionHeader").innerHTML = "Question 13/21";
-            //document.getElementById("imgClickAndChange").src = "data13";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-60.png?raw=true";
             sessionStorage.setItem("buttonClicks", "14")
             break;
         case 14:
             document.getElementById("questionHeader").innerHTML = "Question 14/21";
-            //document.getElementById("imgClickAndChange").src = "data14";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-65.png?raw=true";
             sessionStorage.setItem("buttonClicks", "15")
             break;
         case 15:
             document.getElementById("questionHeader").innerHTML = "Question 15/21";
-            //document.getElementById("imgClickAndChange").src = "data15";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-70.png?raw=true";
             sessionStorage.setItem("buttonClicks", "16")
             break;
         case 16:
             document.getElementById("questionHeader").innerHTML = "Question 16/21";
-            //document.getElementById("imgClickAndChange").src = "data16";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-75.png?raw=true";
             sessionStorage.setItem("buttonClicks", "17")
             break;
         case 17:
             document.getElementById("questionHeader").innerHTML = "Question 17/21";
-            //document.getElementById("imgClickAndChange").src = "data17";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-80.png?raw=true";
             sessionStorage.setItem("buttonClicks", "18")
             break;
         case 18:
             document.getElementById("questionHeader").innerHTML = "Question 18/21";
-            //document.getElementById("imgClickAndChange").src = "data18";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-85.png?raw=true";
             sessionStorage.setItem("buttonClicks", "19")
             break;
         case 19:
             document.getElementById("questionHeader").innerHTML = "Question 19/21";
-            //document.getElementById("imgClickAndChange").src = "data19";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-90.png?raw=true";
             sessionStorage.setItem("buttonClicks", "20")
             break;
         case 20:
             document.getElementById("questionHeader").innerHTML = "Question 20/21";
-            //document.getElementById("imgClickAndChange").src = "data20";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-95.png?raw=true";
             sessionStorage.setItem("buttonClicks", "21")
             break;
         case 21:
             document.getElementById("questionHeader").innerHTML = "Question 21/21";
-            //document.getElementById("imgClickAndChange").src = "data21";
+            document.getElementById("imgClickAndChange").src = "https://github.com/dianamoulton18/WRAMP-code/blob/master/images/pie/pie-100.png?raw=true";
             sessionStorage.setItem("buttonClicks", "22")
             break;
         case 22:
